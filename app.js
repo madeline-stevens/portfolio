fbb1de9cb0575b330722f789f119ba904faccdd3
Madeline Stevens <stevensm1987@gmail.com>
2017-07-11 11:52:27 -0700

    added scaffolding, adding an image place holder, still not mobile first

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 'use strict';
 
 //global variables
+Projects.all=[];
 
+var container = document.getElementByClassName('clickableScreenshot');
 
 //constructor function for projects
-function projects (name, ghpagesAddress){
-  this.name= name;
-  this.ghpagesAddress= ghpagesAddress;
-}
+function Project (name, filepath, link){
+  this.name=name;
+  this.filepath=filepath;
+  this.link=link;
+  Project.all.push(this);
+};
+
+//instances
+// var busmall = new project('busmall', 'images/Screen Shot 2017-07-10 at 11.07.03 PM.png');
