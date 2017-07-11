334a8cbc243fa10d22ac74538f887c4fcfa02e3a
Madeline Stevens <stevensm1987@gmail.com>
2017-07-10 17:48:49 -0700

    adding scaffolding then need to create Monday's branch

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//global variables
+
+
+//constructor function for projects
+function projects (name, ghpagesAddress){
+  this.name= name;
+  this.ghpagesAddress= ghpagesAddress;
+}
