57943dcd021d6e2f6a3f426c7765479cb2e43897
Madeline Stevens <stevensm1987@gmail.com>
2017-07-14 11:58:05 -0700

    hamburger button almost working, adding my rough draft of handlebar rough draft

--- project.js
+++ project.js
@@ -1,48 +1,50 @@
 'use strict';
 
-//global variables
-// Projects.all=[];
+//view object, to hold all our functions for dynamic updates and project-related event handlers
+var projectView = [];
 
+function Article (rawDataObj) {
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.body = rawDataObj.body;
+  this.publishedOn = rawDataObj.publishedOn;
+}
 
+Article.prototype.toHtml = function(){
+  // TODO: Use handlebars to render your articles.
+  //       - Get your template from the DOM.
+  //       - Now "compile" your template with Handlebars.
 
-// //constructor function for projects
-// function Project (rawDataObj){
-//   this.title = rawDataObj.title;
-//   this.date = rawDataObj.date;
-//   this.filepath = rawDataObj.filepath;//photo maybe?
-//   this.link= rawDataObj.link;
-//   // Project.all.push(this);
-// };
-//
-// Projects.prototype.toHtml = function(){
-//   var $newProject = $('project.template').clone();
-//
-//   $newProject.removeClass('template');
-//
-//   $newProject.find('h1').html(this.title);
-//   $newProject.find('date').html(this.date);
-//   $newProject.find('a').html(this.filepath);
-//   $newProject.find('a').html(this.link);
-//
-//   //Display time
-//   $newProject.find('time').html('about ' + parseInt((new Date()-new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-//   $newProject.append('<hr>');
-//   return $newProject;
-// };
-//
-// rawData.sort(function(a,b){
-//   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-// });
-//
-// rawData.forEach(function(projectObject) {
-//   projects.push(new Project(projectObject));
-// });
-//
-// projects.forEach(function(project){
-//   $('#projects').append(project.toHtml());
-// });
+  var newArt = {
+    title: this.title,
+    author: this.author,
+    body: this.body,
+    publishedOn: this.publishedOn,
+    category: this.category,
+  }
 
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
+    // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+    var articleData = $('#articleData').html();
+    var compile = Handlebars.compile(articleData);
+    var newArticle = compile(newArt);
+    $('#articles').append(newArticle)
+  };
 
-//instances
+  rawData.sort(function(a,b) {
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  });
+
+  rawData.forEach(function(articleObject) {
+    articles.push(new Article(articleObject));
+  });
+
+  articles.forEach(function(article){
+    $('#articles').append(article.toHtml());
+  });
+
 // var busmall = new project('busmall', 'images/Screen Shot 2017-07-10 at 11.07.03 PM.png');
