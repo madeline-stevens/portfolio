1d4ff4dff8b67215d594e3d5cc86150edb0bd1b0
Madeline Stevens <stevensm1987@gmail.com>
2017-07-17 11:43:00 -0700

    buttons are clickable between about and all projects, all projects are hidden when about is clicked, mobile collapsed hamburger button still hides all links when expanded

---
+++ scripts/rawData.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var rawData = [
+  {
+  titleOfProject: 'Bus Mall',
+  dateOfProject: 'June',
+  description: 'Product Survey',
+},
+{
+  titleOfProject: 'Bus Mall',
+  dateOfProject: 'June',
+  description: 'Product Survey',
+
+}]
