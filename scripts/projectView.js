9c268be9e38138467cdfc3112d7876a1c3291a82
Madeline Stevens <stevensm1987@gmail.com>
2017-07-18 11:52:17 -0700

    Adding scafforlding for the initNewProjectPage method and the initNewIndexPage method as well as the rough draft of my local storage

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -20,12 +20,44 @@ $(function() {
  $('#home').css('display', 'block');
 });
 
+//where the setTeasers function would go
 
-// $('.main-nav .tab:first').click();//this triggers a click on the first .tab element to set up the the page.
-
-// DONE: Call all of the above functions, once we are sure the DOM is ready.
-// $(document).ready(function() {
 //
-// })
+articleView.initNewProjectPage = function(){
+  $('.tab-content').show();
+  $('#export-field').hide();
+  $('#article-json').on('focus', function(){
+    this.select();
+  });
+  $('#new-form').on('change', 'input, textarea', articleView.create)
+}
+
+projectView. create = function() {
+  let article;
+  $('projects').empty();
+
+//new blog entry stuff
+//   project = new Project({
+//     titleOfProject: $('#project-titleOfProject').val(),
+//     dateOfProject: $('#project-dateOfProject').val(),
+//     description: $('#project-description').val(),
+//     // category: $('#project-title').val(),
+//     // body: $('#project-title').val(),
+//     // publishedOn: $('#project-title').val(),
+//   });
+//   $('#projects').append(project.toHtml());
+//   $('pre code').each(function(i, block){
+//     hljs.highlightBlock(block);
+//   };
+//   $('#export-field').show();
+//   $('#project-json').val('$.........')
+// }
+
+projectView.initIndexPage = function(){
+  Project.all.forEach(function(article){
+    $('#projects').append(article.toHtml())
+  }
+}
+
 
 projectView.handleMainNav();
